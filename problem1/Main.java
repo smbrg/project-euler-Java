7bf85731b4f4b7617ba4f3fe8a19a2f10ac377d4
smbrg <smbrg17@gmail.com>
2017-05-16 19:30:50 -0700

    moving

---
+++ problem1/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int ans = 0;
+        int i;
+        for (i = 0; i < 1000; i++) {
+            if (i%3 == 0) {
+                ans += i;
+            } else if (i%5 == 0) {
+                ans += i;
+            }
+        }
+        System.out.println(ans);        
+    }
+
+}
